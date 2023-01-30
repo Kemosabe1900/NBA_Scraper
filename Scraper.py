1fc279159364cda19b8a636cf1138eabdab50f4b
Kemosame <matiabeya2@gmail.com>
2023-01-30 02:44:37 -0600

    api done

--- Scraper.py
+++ Scraper.py
@@ -1,7 +1,15 @@
+import numpy as np
+import time
 import pandas as pd
 import requests
-# show all columns in a wide DataFrame
-pd.set_option('diplay.max_colums', None)
+from pprint import PrettyPrinter
+import json
 
-test_url = "https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=Totals&Scope=S&Season=2022-23&SeasonType=Regular%20Season&StatCategory=PTS"
-# r = requests.get(url =  ).json
+# show all columns in a wide DataFrame
+# pd.set_option('diplay.max_columns', None)
+
+test_url = 'https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=2022-23&SeasonType=Regular Season&StatCategory=PTS'
+r = requests.get(url=test_url).json()
+table_headers = r['resultSet']['headers']
+print(pd.DataFrame(r['resultSet']['headers']))
+# print(table_headers)
